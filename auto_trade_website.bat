d6f6319c93bc3037da25737dc905010d05aa2220
Hanaroya <kimmo6072@gmail.com>
2024-10-05 16:34:41 +0900

    Update auto_trade_website.bat

--- auto_trade_website.bat
+++ auto_trade_website.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d C:\Users\ASUS\Documents\GitHub\upbit_auto_trade_dev
-start gunicorn -w 4 -b 0.0.0.0:5000 main_flask_server:app
+start waitress-serve --port=5000 main_flask_server:app
 timeout /t 5
 start http://localhost:5000
