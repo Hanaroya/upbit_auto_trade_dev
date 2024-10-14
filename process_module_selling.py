4046727bc38de219144e3589ffccef48d8bbd380
Hanaroya <kimmo6072@gmail.com>
2024-10-14 11:17:56 +0900

    Update process_module_selling.py

--- process_module_selling.py
+++ process_module_selling.py
@@ -52,9 +52,9 @@ def coin_receive_regular_selling():
                 # 투자 전략 계산용 factor 받아오기
                     if trade_factors.iloc[-1]['rsi_K'] >= 80 and trade_factors.iloc[-1]['rsi_D'] >= 80:
                         rsi_S = 'ready'
-                    else: rsi_S = 'standby' 
-                     
-                    if trade_factors.iloc[-1]['rsi_K'] < trade_factors.iloc[-1]['rsi_D'] and rsi_S == 'ready': 
+                    elif trade_factors.iloc[-1]['rsi_K'] <= 70 and trade_factors.iloc[-1]['rsi_D'] <= 70: rsi_S = 'standby'
+                    
+                    if trade_factors.iloc[-1]['rsi_K'] < trade_factors.iloc[-1]['rsi_D'] and trade_factors.iloc[-1]['rsi_D'] > 80 and rsi_S == 'ready': 
                         rsi_S = 'go'
                 except Exception as e:
                     logging.error("Exception 발생!")
@@ -158,8 +158,8 @@ def coin_receive_user_selling():
                 # 투자 전략 계산용 factor 받아오기
                     if trade_factors.iloc[-1]['rsi_K'] >= 80 and trade_factors.iloc[-1]['rsi_D'] >= 80:
                         rsi_S = 'ready'
-                    
-                    if trade_factors.iloc[-1]['rsi_K'] < trade_factors.iloc[-1]['rsi_D'] and rsi_S == 'ready': 
+                    elif trade_factors.iloc[-1]['rsi_K'] <= 70 and trade_factors.iloc[-1]['rsi_D'] <= 70: rsi_S = 'standby'
+                    if trade_factors.iloc[-1]['rsi_K'] < trade_factors.iloc[-1]['rsi_D'] and trade_factors.iloc[-1]['rsi_D'] > 80 and rsi_S == 'ready': 
                         rsi_S = 'go'
                 except Exception as e:
                     logging.error("Exception 발생!")
