86012e521bf11ae96dcb1de9a4f6d65675be8249
Hanaroya <kimmo6072@gmail.com>
2024-10-21 16:53:14 +0900

    Update process_module_buying.py

--- process_module_buying.py
+++ process_module_buying.py
@@ -214,7 +214,7 @@ def buying_process(trade_factors, sma200, c_rank, t_record, total_am:float, curs
     if case2_chk:
         t_record['record']['case2_chk'] = cp
    
-    condition1 = t_record['record']['case1_chk'] > 0 and t_record['hold'] == False and 40 > trade_factors.iloc[-1]['rsi_K'] > trade_factors.iloc[-1]['rsi_D'] and t_record['rsi'] < 50 and macd_check(trade_factors=trade_factors)
+    condition1 = t_record['record']['case1_chk'] > 0 and t_record['hold'] == False and 40 > trade_factors.iloc[-1]['rsi_K'] > trade_factors.iloc[-1]['rsi_D'] and t_record['rsi'] < 50
     condition2 = t_record['record']['case2_chk'] > 0 and t_record['hold'] == False and cp >= t_record['record']['case2_chk'] and t_record['rsi'] > 65
     condition3 = t_record['position'] in checking[1:]
 
