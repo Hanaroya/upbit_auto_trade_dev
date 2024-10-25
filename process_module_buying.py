b504de09fc472e68fdc3e3bc86010ee945e97701
Hanaroya <kimmo6072@gmail.com>
2024-10-25 12:58:50 +0900

    Update process_module_buying.py

--- process_module_buying.py
+++ process_module_buying.py
@@ -202,7 +202,7 @@ def buying_process(trade_factors, sma200, c_rank, t_record, total_am:float, curs
         t_record['record']['case1_chk'] = cp
     if case2_chk:
         t_record['record']['case2_chk'] = cp
-    if t_record['record']['case1_chk'] > 0 and cp < t_record['record']['case1_chk'] and macd_check(data=trade_factors) == False: t_record['record']['case1_chk'] = cp
+    if t_record['record']['case1_chk'] > 0 and cp < t_record['record']['case1_chk'] and macd_check(trade_factors=trade_factors) == False: t_record['record']['case1_chk'] = cp
     
     if (cp > (t_record['record']['case2_chk'] * 1.002)
         ) or (t_record['record']['case2_chk'] > 0 and trade_factors.iloc[-1]['signal'] > trade_factors.iloc[-1]['macd']): 
@@ -213,7 +213,7 @@ def buying_process(trade_factors, sma200, c_rank, t_record, total_am:float, curs
     
     condition1 = t_record['record']['case1_chk'] > 0 and t_record['hold'] == False and (
         50 > trade_factors.iloc[-1]['rsi_K'] > trade_factors.iloc[-1]['rsi_D'] > 25) and (
-            cp >= t_record['record']['case1_chk']) and macd_check(data=trade_factors) == True
+            cp >= t_record['record']['case1_chk']) and macd_check(trade_factors=trade_factors) == True
     condition2 = t_record['record']['case2_chk'] > 0 and t_record['hold'] == False and (
         90 > trade_factors.iloc[-1]['rsi_K'] > trade_factors.iloc[-1]['rsi_D'] > 50 and (
             t_record['record']['case2_chk'] * 1.002)>= cp >= t_record['record']['case2_chk'] and t_record['rsi'] > 65)
