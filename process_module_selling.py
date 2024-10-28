f37c80a8995a7f514757a34667706f76790bd020
Hanaroya <kimmo6072@gmail.com>
2024-10-29 01:21:27 +0900

    Update process_module_selling.py

--- process_module_selling.py
+++ process_module_selling.py
@@ -233,7 +233,7 @@ def case1_check(up_chk_b, rsi_S, ubmi): # 최상의 경우를 염두하고 작�
 def case2_check(t_record, trade_factors, sma200, up_chk_b, ubmi): # 차상의 경우 혹은 몇몇 조건이 불충분한데 이익이 날 경우 
     checker = 0.3
     if ubmi < -20: checker = 0.1
-    if str(t_record['record']['strategy']).find('case 1 B') > -1: checker > 0.8
+    if str(t_record['record']['strategy']).find('case 1 B') > -1: checker = 0.8
     if up_chk_b > checker and trade_factors.iloc[-1]['signal'] > 0:
         if ((trade_factors.iloc[-1]['macd'] < (trade_factors.iloc[-1]['signal'] # MACD가 시그널 보다 낮은데 가격이 높을 경우
             ) or (trade_factors.iloc[-1]['rsi_K'] < (trade_factors.iloc[-1]['rsi_D'] - 5) # rsi_K 값이 rsi_D 값보다 낮은데 가격이 높을 경우
