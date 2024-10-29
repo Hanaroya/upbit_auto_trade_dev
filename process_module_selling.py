87d948f75b17f6ebf2f70c4873dcc2b1fdc47d4d
Hanaroya <kimmo6072@gmail.com>
2024-10-29 09:17:29 +0900

    Update process_module_selling.py

--- process_module_selling.py
+++ process_module_selling.py
@@ -652,7 +652,8 @@ def selling_process(c_list, t_record, sma200, total_am:float, curs, conn): # 가
                 elif case3_chk == True: t_record['record']['strategy'] = 'case 3 S '+ t_record['record']['strategy']
                 elif user_call == False and str(t_record['position']).find('emergency') > -1: t_record['record']['strategy'] = 'case E S ' + t_record['record']['strategy']
                 elif user_call == True: t_record['record']['strategy'] = 'case U S ' + t_record['record']['strategy']
-                elif user_call == False and str(t_record['position']).find('reach profit point') > -1: 'case 1.4 S ' + t_record['record']['strategy']
+                elif user_call == False and str(t_record['position']).find('reach profit point') > -1: 
+                    t_record['record']['strategy'] = 'case 1.4 S ' + t_record['record']['strategy']
 
                 #이익금 정리
                 tm.profit_control(total_am=total_am,deposit=deposit) 
