341db745ff79916f448c5a0bd336425359c2559d
Hanaroya <kimmo6072@gmail.com>
2024-10-29 09:36:21 +0900

    Update process_module_buying.py

--- process_module_buying.py
+++ process_module_buying.py
@@ -213,7 +213,7 @@ def buying_process(trade_factors, sma200, c_rank, t_record, total_am:float, curs
         ) and case1_check(trade_factors=trade_factors) == False: 
         t_record['record']['case1_chk'] = 0
     
-    condition1 = t_record['record']['case1_chk'] > 0 and t_record['hold'] == False and cp < (t_record['record']['case1_chk'] * 0.9999)
+    condition1 = t_record['record']['case1_chk'] > 0 and t_record['hold'] == False and cp > (t_record['record']['case1_chk'] * 1.0008)
     
     condition2 = t_record['record']['case2_chk'] > 0 and t_record['hold'] == False and t_record['record']['case2_chk'] < cp and (
         trade_factors.iloc[-1]['rsi_K'] > trade_factors.iloc[-1]['rsi_D'] > 55) and (
