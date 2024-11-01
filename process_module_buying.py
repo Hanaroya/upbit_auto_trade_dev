c5a3d395ffafe2c80fe6fd16f8ac22911029f6f1
Hanaroya <kimmo6072@gmail.com>
2024-11-01 22:47:11 +0900

    Update process_module_buying.py

--- process_module_buying.py
+++ process_module_buying.py
@@ -217,12 +217,7 @@ def buying_process(trade_factors, sma200, c_rank, t_record, total_am:float, curs
     condition2 = t_record['record']['case2_chk'] > 0 and t_record['hold'] == False and t_record['record']['case2_chk'] < cp and (
         trade_factors.iloc[-1]['rsi_K'] > 45 and trade_factors.iloc[-1]['rsi_D'] > 45) and (
         trade_factors.iloc[-2]['macd'] < trade_factors.iloc[-1]['macd']) and (
-        trade_factors.iloc[-3]['macd'] < trade_factors.iloc[-2]['macd']) and ((
-        trade_factors.iloc[-4]['macd'] < trade_factors.iloc[-4]['signal']) or ( # 최소 2시간 이내에 MACD 크로스 발생 징조가 있었는지 확인
-        trade_factors.iloc[-5]['macd'] < trade_factors.iloc[-5]['signal']) or (
-        trade_factors.iloc[-6]['macd'] < trade_factors.iloc[-6]['signal']) or (
-        trade_factors.iloc[-7]['macd'] < trade_factors.iloc[-7]['signal']) or (
-        trade_factors.iloc[-8]['macd'] < trade_factors.iloc[-8]['signal']))
+        trade_factors.iloc[-3]['macd'] < trade_factors.iloc[-2]['macd'])
     condition3 = t_record['position'] in checking[1:]
     
     # 실제 구매 할지 블랙리스트 호출
